8a9ded814e036068e3c6d99fc93f024113d79451
Claire HALLIDAY <clairehalliday73@gmail.com>
2022-02-25 16:34:16 +0100

    second commit

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { ProductListComponent } from '../product-list/product-list.component';
+import { Product } from '../products';
 
 @Component({
   selector: 'app-product-alerts',
   templateUrl: './product-alerts.component.html',
   styleUrls: ['./product-alerts.component.css']
 })
-export class ProductAlertsComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class ProductAlertsComponent extends ProductListComponent {
+  @Input() product: Product | undefined;
+  @Output() notify = new EventEmitter();
 }
